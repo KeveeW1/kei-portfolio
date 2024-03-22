5a1ba13c4fb0c0a0f21101c1d2b26115ed195ca4
Keira <1keira.kw@gmail.com>
2024-03-21 22:49:23 -0700

    Fixing package installation?

--- src/app/pages/Hero.jsx
+++ src/app/pages/Hero.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import Image from 'next/image';
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 import './Hero.css';
 
 const Hero = () => {
