b74cd1b9e7d9b4bc109868c40e6df21ef9f224c6
Keira <1keira.kw@gmail.com>
2024-03-31 15:24:16 -0700

    Added more archives in Archive page

--- src/app/archive/page.jsx
+++ src/app/archive/page.jsx
@@ -11,7 +11,7 @@ const Archive = () => {
         <h1 className='text-3xl'>&lt;archive/&gt;</h1>
         <h4 className=''><i>hi, welcome to keira&apos;s archive!</i></h4>
         <br></br>
-        <h1 className='text-2xl text-left pl-10 text-blue-500'>awards</h1>
+        <h1 className='text-2xl text-left pl-10 text-blue-400'>hackathons</h1>
         <div className='text-left pl-10'>
           <li>INRIX Amazon University Hack 2023 Finalist – 2023</li>
           <li>DubHacks 2023 Winner – 2023</li>
@@ -23,31 +23,44 @@ const Archive = () => {
           <li>Microsoft “Girls in AI” Hackathon: 2nd Place Winner – 2019</li>
         </div>
 
+  
+        <h1 className='pt-2 text-2xl text-left pl-10 text-blue-500'>awards</h1>
+        <div className='text-left pl-10'>
+          <p>• National Cyber Scholarship Foundation (NCSF): Bronze Award – 2020 / Semifinalist – 2021 / Finalist – 2022 / Scholar Award – 2022
+<br></br>• Women in Cybersecurity (WiCyS): Top 1% of High School Scholar Award – 2022
+<br></br>• UN Global Goal National Community Service: Ambassador Award – 2022
+<br></br>• Kode With Klossy: Scholar Award – 2021 / Alumni and Scholar Award – 2022
+<br></br>• Titan Robotics Club: Mentors' Award and Varsity Letter Award – 2021
+<br></br>• National Center for Women & Information Technology (NCWIT): Aspirations in Computing Award – 2021</p>
+          
+        </div>
+      
       <div>
-        <h1 className='text-2xl pl-10 pt-5 text-center pb-2 text-blue-700'>publication + speakers</h1>
-        <div className='text-left pl-10'></div>
-        <li>Speaker &quot;Impact in My Community&quot; hosted by Bill and Melinda Gates Foundation Teen Lounge Event – 2023</li>
-        <li>Published by Journal of Interdisciplinary Public Policy - &quot;ReNews: A Real Solution to Fake&quot; – 2022</li>
-        <li>Panelist &quot;Experiences and challenges in the beautiful world of Computer Science by NCWIT&quot; – 2022</li>
-        <li>Speaker, “National Day of Solidarity Against AAPI Hate” rally in Bellevue Downtown, sponsored by Pathway Foundation – 2021</li>
+        <h1 className='pt-2 text-2xl text-left pl-10 text-blue-600'>publication + speakers</h1>
+        <div className='text-left pl-10'>
+        <p>• Speaker &quot;Impact in My Community&quot; hosted by Bill and Melinda Gates Foundation Teen Lounge Event – 2023
+        <br></br> • Published by Journal of Interdisciplinary Public Policy - &quot;ReNews: A Real Solution to Fake&quot; – 2022
+        <br></br> • Panelist &quot;Experiences and challenges in the beautiful world of Computer Science by NCWIT&quot; – 2022
+        <br></br> • Speaker, “National Day of Solidarity Against AAPI Hate” rally in Bellevue Downtown, sponsored by Pathway Foundation – 2021</p>
+        </div>
       </div>
 
       <div>
-        <h1 className='text-2xl pt-5 pb-2'>past experiences</h1>
-        <div className='text-left flex justify-center'>
+        <h1 className='pt-2 text-2xl text-left pl-10 text-blue-800'>past experiences</h1>
+        <div className='text-left pl-10'>
           <p>Titan Robotics Club, Executive/Software Programmer/Mentor/Outreach Head, Redmond, WA<br></br>
-            ● Designed, built program, and debugged robot for the national FIRST Robotics Competitions<br></br>
-            ● Instructed and mentored a group of 80+ middle school students in robotics programming<br></br>
-            ● Managed multiple competitions simultaneously through effective collaboration<br></br>
-            ● Identified, drove, and implemented improvements in robot operations resulting in a score increase of 20%<br></br><br></br>
+            • Designed, built program, and debugged robot for the national FIRST Robotics Competitions<br></br>
+            • Instructed and mentored a group of 80+ middle school students in robotics programming<br></br>
+            • Managed multiple competitions simultaneously through effective collaboration<br></br>
+            • Identified, drove, and implemented improvements in robot operations resulting in a score increase of 20%<br></br><br></br>
             International School of Bellevue, Founder and President of Civic Club/IS Hack Club, Bellevue, WA<br></br>
-            ● Provided student-centered educational projects including leadership growth and community engagement<br></br>
-            ● Partnered with Microsoft to drive a variety of Civic and Community events including voter registration and annual debate on current<br></br>
+            • Provided student-centered educational projects including leadership growth and community engagement<br></br>
+            • Partnered with Microsoft to drive a variety of Civic and Community events including voter registration and annual debate on current<br></br>
             issues<br></br>
-            ● Hosted hackathon to promote cybersecurity awareness through a highly stimulative learning path<br></br><br></br>
+            • Hosted hackathon to promote cybersecurity awareness through a highly stimulative learning path<br></br><br></br>
             Pathway Foundation Civic and Social, Information Technology Lead/Outreach Executive, Bellevue, WA 2019 – Present<br></br>
-            ● Served as technical lead to support the organization’s strategic planning and execution<br></br>
-            ● Mentored 30+ underserved students in Python programming year-round<br></br>
+            • Served as technical lead to support the organization&apos;s strategic planning and execution<br></br>
+            • Mentored 30+ underserved students in Python programming year-round<br></br>
             </p>
         </div>
       </div>
